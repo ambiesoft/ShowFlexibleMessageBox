638180d10f3a382d7a7e54c00a020d458ab6729f
ambiesoft <ambiesoft.trueff@gmail.com>
2022-02-23 22:16:02 +0900

    first

---
+++ ShowFlexibleMessageBox/Program.cs
@@ -0,0 +1,30 @@
+﻿using JR.Utils.GUI.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Ambiesoft.ShowFlexibleMessageBox
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            string a = HttpUtility.UrlDecode("%3c");
+            string message = args.Length == 0 ? "No Message" :
+                HttpUtility.UrlDecode(Encoding.UTF8.GetBytes(args[0]), Encoding.UTF8);
+
+            FlexibleMessageBox.Show(message);
+        }
+    }
+}
