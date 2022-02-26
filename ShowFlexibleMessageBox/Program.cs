a29ed98ccef6ceaacbf615a25dc73b1fc8095e51
ambiesoft <ambiesoft.trueff@gmail.com>
2022-02-26 20:09:08 +0900

    Takes title in commnad line

--- ShowFlexibleMessageBox/Program.cs
+++ ShowFlexibleMessageBox/Program.cs
@@ -1,4 +1,5 @@
-﻿using JR.Utils.GUI.Forms;
+﻿using CommandLine;
+using JR.Utils.GUI.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,14 @@ namespace Ambiesoft.ShowFlexibleMessageBox
 {
     internal static class Program
     {
+        public class Options
+        {
+            [Option('t', "title", Required = false, HelpText = "Title of message box")]
+            public string Title { get; set; }
+
+            [CommandLine.Value(0)]
+            public string MainText { get; set; }
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -20,10 +29,14 @@ namespace Ambiesoft.ShowFlexibleMessageBox
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            string message = args.Length == 0 ? "No Message" :
-                HttpUtility.UrlDecode(Encoding.UTF8.GetBytes(args[0]), Encoding.UTF8);
+            Parser.Default.ParseArguments<Options>(args)
+             .WithParsed<Options>(o =>
+             {
+                 string message = string.IsNullOrEmpty(o.MainText) ? "No Message" :
+                     HttpUtility.UrlDecode(Encoding.UTF8.GetBytes(o.MainText), Encoding.UTF8);
 
-            FlexibleMessageBox.Show(message);
+                 FlexibleMessageBox.Show(message, o.Title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+             });
         }
     }
 }
